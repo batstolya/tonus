a585855b72d44517cd50de2cd7f153ab3feca0d8
anatolii <batstolya@gmail.com>
2026-08-09 16:42:04 +0200

    feat(web): add avatar account menu
    (cherry picked from commit 210eb9ac5257d21b88b3b17182323713e69978e9)

--- apps/web/src/lib/noEmoji.test.ts
+++ apps/web/src/lib/noEmoji.test.ts
@@ -51,6 +51,7 @@ const PILOT_FILES = [
   'components/landing/blocks/TrustStrip.tsx',
   'components/auth/AuthScreen.tsx',
   'components/ui/Avatar.tsx',
+  'components/ui/TopbarAvatar.tsx',
 ]
 
 const REPLACED = Object.values(ICONS).map(e => e.emoji)
